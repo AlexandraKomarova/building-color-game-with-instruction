ab6ca770ba8547feea0fed366c0546cdd5b8b679
Sonechka M <s212288s@gmail.com>
2019-12-27 21:18:53 -0500

    added a function to change the color of all squares to the correct color when correct color is picked

--- script.js
+++ script.js
@@ -25,10 +25,20 @@ for(var i = 0; i < squares.length; i++){
         //compare color to pickedColor
         if(clickedColor === pickedColor){
             messageDisplay.textContent = "Correct!";
+            changeColors(clickedColor);
         } else {
             this.style.backgroundColor = "#232323";
             messageDisplay.textContent = "Try Again!";
         } 
-
     });
+}
+
+function changeColors(color){
+    // loop through all squares
+    for(var i = 0; i <squares.length; i++){
+        // change color to match given color
+        squares[i].style.backgroundColor = color;
+    }
+    
+
 }
